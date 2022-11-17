b8dd5922e1f44016860974e0f1f9e5f47133ebaf
Rebecca Boateng <r.y.boateng@outlook.com>
2022-11-17 23:27:04 +0000

    fix timeline content

--- js/index.js
+++ js/index.js
@@ -50,36 +50,36 @@ for (let counter =0; counter < dates.length; counter++){
         // box with content
 
         
-        const div3 = document.createElement("div");
-        div3.className = "timeline_item";
+        // const div3 = document.createElement("div");
+        // div3.className = "timeline_item";
 
-        const title = document.createElement("h2");
-        title.className = "timeline-item-title";
-        title.innerHTML = dates[counter].title;
-        div3.appendChild(title);
+        // const title = document.createElement("h2");
+        // title.className = "timeline-item-title";
+        // title.innerHTML = dates[counter].title;
+        // div3.appendChild(title);
 
-        const summaryText = document.createElement("p");
-        summaryText.className = "timeline-item-summary";
-        summaryText.innerHTML = dates[counter].summary;
-        div3.appendChild(summaryText);
+        // const summaryText = document.createElement("p");
+        // summaryText.className = "timeline-item-summary";
+        // summaryText.innerHTML = dates[counter].summary;
+        // div3.appendChild(summaryText);
 
-        const infoButton = document.createElement("button");
-        infoButton.className = "timeline-item-more-info";
-        infoButton.innerHTML = "More info";
-        div3.appendChild(infoButton);
-        container.appendChild(div3);
+        // const infoButton = document.createElement("button");
+        // infoButton.className = "timeline-item-more-info";
+        // infoButton.innerHTML = "More info";
+        // div3.appendChild(infoButton);
+        // container.appendChild(div3);
 
-        // line in the middle
-        const div2 = document.createElement("div");
-        div2.className = "timeline__middle";
-        container.appendChild(div2);
+        // // line in the middle
+        // const div2 = document.createElement("div");
+        // div2.className = "timeline__middle";
+        // container.appendChild(div2);
 
 
-        // date box
-        const span =  document.createElement("span");
-        span.className= "timeline-item-date";
-        span.innerHTML = dates[counter].date;
-        container.appendChild(span);
+        // // date box
+        // const span =  document.createElement("span");
+        // span.className= "timeline-item-date";
+        // span.innerHTML = dates[counter].date;
+        // container.appendChild(span);
       
     }
 };
