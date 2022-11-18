e1db1d6ff8511e05534f7d23ee4e4ea7e8017c9d
JazHarlow <Jazreenaharlow@gmail.com>
2022-11-18 17:46:38 +0000

    fix timeline DOM

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
 import { dates } from "./data.js";
 
-const timeline = document.querySelector(".timeline");
-
 const header = document.createElement("h1");
 header.innerHTML = "MAYME'S BAKERY TIMELINE";
 document.body.appendChild(header);
+
+
+const timeline = document.querySelector(".timeline");
 
 for (let counter = 0; counter < dates.length; counter++) {
   
   const div = document.createElement("div");
   div.className = "timeline-item";
-  // timeline.appendChild(div)??
+  document.body.appendChild(div)
 
   const title = document.createElement("h2");
   title.className = "timeline-item-title";
@@ -31,67 +32,72 @@ for (let counter = 0; counter < dates.length; counter++) {
   infoButton.className = "timeline-item-more-info";
   infoButton.innerHTML = "More info";
   document.body.appendChild(infoButton);
-  timeline.appendChild(div)
+  timeline.appendChild(div);
 
-  //Modal
+  //MODAL
 
-  const modal = document.createElement("div");
-  modal.setAttribute('id','modal');
-  modal.className = "modal";
-  document.body.appendChild(modal)
-  
+  // const modal = document.createElement("div");
+  // modal.setAttribute('id','modal');
+  // modal.className = "modal";
+  // document.body.appendChild(modal)
 
   const modalId = document.createElement("div");
   modalId.setAttribute('id','modal-container');
-  document.body.appendChild(modalId)
+  modalId.style.display = 'none';
+  document.body.appendChild(modalId);
   
-
   const timeId = document.createElement("modal-date");
   timeId.setAttribute('id','modal-date');
-  timeId.innerHTML = dates[counter].date
-  modalId.appendChild(timeId)
-
+  timeId.innerHTML = dates[counter].date;
+  modalId.appendChild(timeId);
 
   const titleId = document.createElement("modal-title");
   titleId.setAttribute('id','modal-title');
-  titleId.innerHTML = dates[counter].title
-  modalId.appendChild(titleId)
+  titleId.innerHTML = dates[counter].title;
+  modalId.appendChild(titleId);
   
-
   const imageId = document.createElement("img");
   imageId.setAttribute('id','modal-image');
-  imageId.src = dates[counter].image
-  modalId.appendChild(imageId)
+  imageId.src = dates[counter].image;
+  modalId.appendChild(imageId);
   
   const description = document.createElement("p");
   description.setAttribute('id','modal-full-description');
   description.innerHTML = dates[counter].fullDescription
-  modalId.appendChild(description)
+  modalId.appendChild(description);
+
 
   const close = document.createElement("span");
   close.setAttribute('id','modal-close-button')
   close.className = "close";
   close.innerHTML = "&times;"
-  modalId.appendChild(close)
-};
+  modalId.appendChild(close);
+  timeline.appendChild(modalId)
+
+
+// for (let counter = 0; counter < dates.length; counter++) {
 
 // Get DOM Elements
-const modal = document.querySelector("#modal");
+const modalBtn = document.querySelector("#modal-container");
 const openModalBtn = document.querySelector(".timeline-item-more-info");
 const closeModalBtn = document.querySelector(".close");
+
+// openModalBtn.onclick = () => openModal(dates[counter]);
 
 openModalBtn.addEventListener("click", openModal);
 closeModalBtn.addEventListener("click", closeModal);
 
-function openModal() { 
-  modal.style.display = 'block';
+function closeModal()
+ {modalBtn.style.display = 'none';}
+
+function openModal() 
+{ modalBtn.style.display = 'block';};
+
 };
 
-function closeModal() {
-  modal.style.display = 'none';
-}
 
 
 
+  
 
 
